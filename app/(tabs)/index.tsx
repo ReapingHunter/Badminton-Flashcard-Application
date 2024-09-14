733a95ac34af3cae38507771e84e9b83f5dd42c9
ReapingHunter <manalilimarlex@gmail.com>
2024-09-14 20:25:06 +0800

    Added buttons for play and shuffle. Improved UI a bit.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,79 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-import { Text } from 'react-native';
-import { View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Button, TouchableOpacity } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
+type FlashcardData = [{
+  question: string,
+  answer: string,
+}]
+
+const data : FlashcardData = [
+  {
+    question: "A stroke made on the nonracquet side of the body",
+    answer: "Backhand",
+  },
+  {
+    question: "Any infraction of the rules which results in the loss of a serve or in a point for the server",
+    answer: "Fault",
+  },
+  {
+    question: "The object which is volleyed back and forth over the net",
+    answer: "Shuttlecock",
+  },
+  {
+    question: "An overhead stroke hit downward with great velocity and angle",
+    answer: "Smash",
+  },
+  {
+    question: "If the shuttle hits the net and still lands in the proper court is it a Legal or Illegal serve?",
+    answer: "Legal",
+  },
+  {
+    question: "When is the serve in singles made from the right service court?",
+    answer: "When the server's score is an even number",
+  },
+  {
+    question: "What are the 5 basic groups of shots?",
+    answer: "serves, clears, smash, blocks, drop shots",
+  },
+  {
+    question: "What is the mid-line separating the service courts?",
+    answer: "Center line",
+  },
+  {
+    question: "A high, deep serve landing near the long service line in doubles or back boundary line in singles",
+    answer: "Long Serve",
+  },
+  {
+    question: "Badminton became an olympic sport in what year?",
+    answer: "1992",
+  }
+]
 export default function HomeScreen() {
   return (
+    
     <View style={styles.body}>
+      <StatusBar 
+        backgroundColor="#000000"
+        barStyle="light-content"
+      />
       <View style={styles.header}>
-        <Text style={styles.title}>Flashcards</Text>
+        <Text style={styles.title}>Badminton Flashcards</Text>
       </View>
       <View style={styles.container}>
         <View style={styles.page}>
-          <Text style={styles.pageNum}>1 / 149</Text>
+            <Text style={styles.pageNum}>1 / 149</Text>
         </View>
         <View style={styles.flashcard}>
           <Text>Hello World</Text>
+        </View>
+        <View style={styles.horizontalLine} />
+        <View style={styles.actions}>
+          <TouchableOpacity style={styles.iconButton}>
+            <Ionicons name="play" size={60} color={"white"}/>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.iconButton}>
+            <Ionicons name="shuffle" size={60} color={"white"}/>
+          </TouchableOpacity>
         </View>
       </View>
     </View>
@@ -22,16 +82,19 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   header: {
-    backgroundColor: "blue",
+    backgroundColor: "#9cec7f",
     width: "100%",
-    height: "15%",
-    justifyContent: "center",
-    alignItems: "center"
+    height: "20%",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    elevation: 10,
+    zIndex: 1,
   },
   title: {
-    color: "white",
+    color: "#3e752b",
     fontWeight: "bold",
-    fontSize: 60,
+    fontSize: 25,
+    marginBottom: 35,
   },
   body: {
     flex: 1,
@@ -39,7 +102,7 @@ const styles = StyleSheet.create({
     alignItems: "center"
   },
   page: {
-    backgroundColor: "#dedede",
+    backgroundColor: "transparent",
     height: 50,
     width: "80%",
     justifyContent: "center",
@@ -53,17 +116,40 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "#dedede",
     flex: 1,
+    flexDirection: "column",
     width: "100%",
     justifyContent: "center",
     alignItems: "center",
   },
   flashcard: {
     backgroundColor: "white",
-    height: "70%",
+    height: "40%",
     width: "80%",
     justifyContent: "center",
     alignItems: "center",
     borderRadius: 10,
     elevation: 5,
-  }
+    margin: 10,
+  },
+  actions: {
+    height: "20%",
+    width: "90%",
+    justifyContent: "space-evenly",
+    flexDirection: "row",
+    margin:10,
+  },
+  iconButton: {
+    backgroundColor: "#3e752b",
+    borderRadius: 80,
+    height: 80,
+    width: 80,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  horizontalLine: {
+    width: '80%',
+    height: 0.75,
+    backgroundColor: '#9c9c9c',
+    marginVertical: 10,
+  },
 });
