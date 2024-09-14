14b703a5061a421bc5257bea1a38c0c6aafa1b95
L0rd-Inquisit0r <sean.s.l.duran@gmail.com>
2024-09-14 23:12:26 +0800

    importing useRef from react

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import { StyleSheet, Text, View, StatusBar, TouchableOpacity, Animated } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
